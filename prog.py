75f4ddf502fc3e6328ec3f04e6544dc636698912
st084331 <st084331@student.spbu.ru>
2021-01-04 14:34:48 +0300

    Initial commit

---
+++ prog.py
@@ -0,0 +1,57 @@
+import random
+import numpy as np
+from sklearn.cluster import KMeans
+import math
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+class Cluster():
+
+    # K-Means
+    def clusterring(xy, orig_cls, n):
+        kmeans = KMeans(n_clusters=n)
+        kmeans.fit(xy)
+        labels = kmeans.predict(xy)
+        centroids = kmeans.cluster_centers_
+        cls = []
+        for i in range(n):
+            cls.append([])
+        # Для оценки эффективности находим точки пересечения
+        for point in xy:
+            min_dist = float('inf')
+            dists = []
+            i = 0
+            for centroid in centroids:
+                dist = math.sqrt(((point[0] - centroid[0]) ** 2) + ((point[1] - centroid[1]) ** 2))
+                dists.append(dist)
+                if dist < min_dist:
+                    min_dist = dist
+            p = 0
+            for dist in dists:
+                if dist == min_dist:
+                    cls[i].append(point)
+                    p += 1
+                    if p > 1:
+                        print(point, '- точка касания кластеров')
+                i += 1
+        k = 0
+        for cl in orig_cls:
+            for c in cls:
+                if cl == c:
+                    print('Одинаковые кластеры')
+                    print(cl)
+                    print(c)
+                    k += 1
+        if k >= n:
+            print("K-means работает отлично")
+        else:
+            for cl in cls:
+                print(cl)
+        # Иерархическая кластеризация
+        clusters = linkage(xy, method='ward')
+        dendrogram(clusters)
+        plt.show()
+
+
+if __name__ == '__main__':
+    print('Hi')
---
+++ test.py
@@ -0,0 +1,67 @@
+from prog import Cluster
+import random
+# Тест на случайных точках
+print('test1')
+xy = []
+for i in range(1000):
+    xy.append([random.randint(-100,100), random.randint(-100,100)])
+#Cluster.clusterring(xy, [], random.randint(2,20))
+
+# Тестируем на простых квадратах
+print('test2')
+c1 = []
+c2 = []
+c3 = []
+for i in range(3):
+    for j in range(3):
+        c1.append([2+i,2+j])
+        c2.append([6 + i, 6 + j])
+        c3.append([8 + i, 2 + j])
+xy = c1 + c2 + c3
+cls = [c1,c2,c3]
+#Cluster.clusterring(xy, cls, 3)
+
+# Ромб, треугольник, квадрат
+print('test3')
+rhombus = [[0,3],[1,2],[2,3],[1,4]]
+square = []
+for i in range(3):
+    for j in range(3):
+        if i == 1:
+            square.append([3+j, 2+i])
+            j+=1
+        else:
+            square.append([3+j, 2+i])
+triangle = [[1,1], [3,1], [2,2]]
+for i in range(5):
+    triangle.append([i,0])
+xy = rhombus + square + triangle
+cls = [rhombus, square, triangle]
+#Cluster.clusterring(xy, cls, 3)
+
+# Окружность с точкой внутри
+print('test4')
+circle = [[5,0],[-5,0],[0,5],[0,-5],[3,4],[-3,-4],[4,3],[-4,-3]]
+point = [[0,0]]
+xy = circle + point
+cls = [circle, point]
+#Cluster.clusterring(xy,cls,2)
+
+# "Маленькие" данные
+print('test5')
+tr1 = []
+tr2 = []
+d = []
+for i in range(10):
+    d.append([0, 0])
+    d.append([1, -1])
+    d.append([-1, 1])
+    tr1.append([1,1])
+    tr1.append([1,0])
+    tr1.append([0,1])
+    tr2.append([-1,-1])
+    tr2.append([-1,0])
+    tr2.append([0,-1])
+xy = tr1 + tr2 + d
+cls = [tr1,tr2,d]
+Cluster.clusterring(xy, cls, 3)
